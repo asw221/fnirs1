6a833713189d8cbbcb677bf8bf5527f607eb4d91
Andrew Whiteman <awhitem@umich.edu>
2021-08-23 13:28:31 -0400

    Change to write_matrix precision

--- +fnirs1/private/write_matrix.m
+++ +fnirs1/private/write_matrix.m
@@ -1,6 +1,8 @@
 
-function success = write_matrix(M, filename)
+function success = write_matrix(M, filename, varargin)
 % Write matrix M to output file filename
+% Optional third argument specifies the number of decimal places to write.
+% Default is 15
 success = false;
 if (~isnumeric(M))
     error("Input M must be numeric-type");
@@ -8,12 +10,18 @@ end
 if (~(ischar(filename) || isstring(filename)))
     error("Input filename must be a valid path name");
 end
+if (nargin <= 2)
+    digits = 15;
+else
+    digits = max(fix(varargin{1}), 1);
+end
 fid = fopen(filename, 'w+');
+fmt = sprintf('%%.%if', digits);
 if (fid ~= -1)
     [nrow, ncol] = size(M);
     for i = 1:nrow
         for j = 1:ncol
-            fprintf(fid, '%f', M(i, j));
+            fprintf(fid, fmt, M(i, j));
             if (j == ncol)
                 fprintf(fid, '\n');
             else
